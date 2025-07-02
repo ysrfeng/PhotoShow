87a322c930d487e8f3e8a4af46a3916f9632359b
nanfeng <yangshirong@logory.com>
2025-07-02 09:40:10 +0800

    tiaoshi

--- demo/src/main/java/cn/bingoogolapple/photopicker/demo/App.java
+++ demo/src/main/java/cn/bingoogolapple/photopicker/demo/App.java
@@ -11,14 +11,23 @@ import com.squareup.leakcanary.LeakCanary;
  * 描述:
  */
 public class App extends Application {
-
+    /**
+     * 当前的context对象
+     */
+    public static App context;
     @Override
     public void onCreate() {
         super.onCreate();
+        context = this;
         if (LeakCanary.isInAnalyzerProcess(this)) {
             Log.d("App", "In LeakCanary Analyzer Process");
             return;
         }
         LeakCanary.install(this);
     }
+
+    public static App getContext() {
+        return context;
+    }
+
 }
